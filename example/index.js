b814ec7d21bcec2c6386e258c6d70936edda1b0f
Mark Wylde <me@markwylde.co.uk>
2019-11-06 19:39:18 +1000

    Highlight active menu

--- example/index.js
+++ example/index.js
@@ -17,32 +17,40 @@ routeModule({
   fastn.Model.set(state, 'route', route)
 })
 
-const links = () => fastn('ul', {class: 'menu'},
-  fastn('li', fastn('a', {href: '/'}, 'Home')),
-  fastn('li', fastn('a', {href: '/second'}, 'Second')),
-  fastn('li', fastn('a', {href: '/third'}, 'Third')),
+const isActive = (test) =>
+  fastn.binding('route', route => route === test && 'active')
+
+const links = () => fastn('ul', { class: 'menu' },
+  fastn('li', { class: isActive('/') },
+    fastn('a', { href: '/' }, 'Home')),
+
+  fastn('li', { class: isActive('/second') },
+    fastn('a', { href: '/second' }, 'Second')),
+
+  fastn('li', { class: isActive('/third') },
+    fastn('a', { href: '/third' }, 'Third'))
 )
 
-const homePage = () => fastn('section', 
+const homePage = () => fastn('section',
   links(),
   fastn('h1', 'Home Page'),
-  fastn('p', 
+  fastn('p',
     'This is an example of using the router'
   )
 )
 
-const secondPage = () => fastn('section', 
+const secondPage = () => fastn('section',
   links(),
   fastn('h1', 'Second Page'),
-  fastn('p', 
+  fastn('p',
     'This is the second page'
   )
 )
 
-const thirdPage = () => fastn('section', 
-  links(), 
+const thirdPage = () => fastn('section',
+  links(),
   fastn('h1', 'Third Page'),
-  fastn('p', 
+  fastn('p',
     'This is the third page'
   )
 )
@@ -64,7 +72,7 @@ const ui = fastn('templater', {
 
       case '/third':
         return thirdPage()
-  
+
       default:
         return notFoundPage()
     }

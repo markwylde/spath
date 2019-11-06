4e8c399e6c25c1203ca8fdeb67adeee083a54a7a
Kory Nunn <knunn187@gmail.com>
2019-11-07 00:27:26 +1000

    An example of how this might be simplified

---
+++ example/app.js
@@ -0,0 +1,11 @@
+const routering = require('../lib/index')
+const onUrlChange = require('on-url-change');
+
+module.exports = function(mutate){
+  const state = {}
+
+  const emitter = onUrlChange();
+  emitter.on('change', () => mutate.set(state, 'route', window.location.pathname))
+
+  return state
+}

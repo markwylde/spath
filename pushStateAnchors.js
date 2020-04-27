92bfe7fb9fcc8597725fed9d99fdc19db3bced18
Mark Wylde <markwylde@users.noreply.github.com>
2020-04-28 08:19:04 +1000

    Find closest A to fix nesting bug

--- pushStateAnchors.js
+++ pushStateAnchors.js
@@ -1,11 +1,16 @@
 const setPath = require('./setPath');
 
 function pushStateAnchors (event) {
-  const href = event.target.getAttribute('href');
+  const closestAnchor = event.target.closest('a');
+  if (!closestAnchor) {
+    return
+  }
 
+  const href = closestAnchor.getAttribute('href');
+  
   if (href && !href.match(/.*?\/\//)) {
     event.preventDefault();
-    setPath(href);
+    alert('yes')
   }
 }
 
